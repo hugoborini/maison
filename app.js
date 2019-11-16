ecebd7d9a0241225b1235884c5e5b8efb2f5f753
hugo borini <hborini@localhost.localdomain>
2019-11-16 10:33:13 -0500

    first push

---
+++ app.js
@@ -0,0 +1,5 @@
+var menu = document.getElementById("menu");
+var opener = document.getElementById("open-menu");
+opener.addEventListener("click", function() {
+  menu.classList.toggle("is-open");
+});
