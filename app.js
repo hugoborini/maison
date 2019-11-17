a7e968a94001f5344ae3562ab03ed039c809123c
hugo borini <hborini@localhost.localdomain>
2019-11-17 10:57:06 -0500

    finis site

--- app.js
+++ app.js
@@ -3,3 +3,33 @@ var opener = document.getElementById("open-menu");
 opener.addEventListener("click", function() {
   menu.classList.toggle("is-open");
 });
+
+var img = document.getElementById("img");
+
+img.addEventListener("click", function() {
+  img.classList.toggle("is-click");
+});
+
+var img2 = document.getElementById("img2");
+
+img2.addEventListener("click", function() {
+  img2.classList.toggle("is-click");
+});
+
+var img3 = document.getElementById("img3");
+
+img3.addEventListener("click", function() {
+  img3.classList.toggle("is-click");
+});
+
+var img4 = document.getElementById("img4");
+
+img4.addEventListener("click", function() {
+  img4.classList.toggle("is-click");
+});
+
+var img5 = document.getElementById("img5");
+
+img5.addEventListener("click", function() {
+  img5.classList.toggle("is-click");
+});
