57e1af00aeebb635d2d7973289a6eef7d49fec54
hugo borini <hborini@localhost.localdomain>
2019-12-02 17:30:42 -0500

    fix js

--- app.js
+++ app.js
@@ -4,43 +4,28 @@ opener.addEventListener("click", function() {
   menu.classList.toggle("is-open");
 });
 
-var img = document.getElementById("img");
-
-img.addEventListener("click", function() {
-  img.classList.toggle("is-click");
-});
-
-var img2 = document.getElementById("img2");
-
-img2.addEventListener("click", function() {
-  img2.classList.toggle("is-click");
-});
-
-var img3 = document.getElementById("img3");
-
-img3.addEventListener("click", function() {
-  img3.classList.toggle("is-click");
-});
-
-var img4 = document.getElementById("img4");
-
-img4.addEventListener("click", function() {
-  img4.classList.toggle("is-click");
-});
-
-var img5 = document.getElementById("img5");
-
-img5.addEventListener("click", function() {
-  img5.classList.toggle("is-click");
-});
-
+let img = document.querySelectorAll(".img");
+let img_s = document.querySelectorAll(".img_s");
 var article_tab = document.querySelectorAll(".display");
+
+for (let i = 0; i < img_s.length; i++) {
+  img_s[i].addEventListener("click", function() {
+    img_s[i].classList.toggle("is-click");
+  });
+}
+
+for (let i = 0; i < img.length; i++) {
+  img[i].addEventListener("click", function() {
+    img[i].classList.toggle("is-click");
+  });
+}
 
 article_tab.forEach(element => {
   var waypoint = new Waypoint({
     element: element,
     handler: function(direction) {
       element.classList.add("is-reached");
+      console.log("scroll");
     },
     offset: "50%"
   });
